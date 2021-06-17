c827eb7d890e3b3dd315a0f42cf34ddf248fd5d4
Pranjal-pandey-git <85740792+Pranjal-pandey-git@users.noreply.github.com>
2021-06-17 11:49:09 +0530

    Add files via upload

---
+++ MCA4 Answers/23-1901015-PranjalPandey/QUESTION2.py
@@ -0,0 +1,20 @@
+dictionary = [
+      {'employee_id': 10110, 'name': 'James Parkes', 'Dept': 'IT'},
+      {'employee_id':  10111, 'name': 'Lisa Hayden', 'Dept': 'Accounts'}, 
+      {'employee_id': 10113, 'name': 'Brian Adam', 'Dept': 'HR'},
+      {'employee_id': 10114, 'name': 'Rine Arthur', 'Dept': 'Accounts'}, 
+      {'employee_id': 10115, 'name': 'Jeff Simson', 'Dept': 'HR'}
+]
+def find(dictt, key, value):
+    if any(sub[key] == value for sub in dictt):
+        return True
+    return False
+
+
+print( find(dictionary, 'name', 'pranjal pandey'))
+print( find(dictionary, 'name', 'James Parkes'))
+print( find(dictionary, 'Dept', 'IT'))
+print( find(dictionary, 'employee_id', 10110))
+print( find(dictionary, 'employee_id', 1011719))
+
+
